f5fe3300f50298b7cff76317c6b5d3d07db93cf2
Mikołaj Wolski <mwolski16@gmail.com>
2022-09-11 02:11:42 +0200

    Started adding custom polygon drawing

--- src/components/LoginPage/MainPage/Map/Map.tsx
+++ src/components/LoginPage/MainPage/Map/Map.tsx
@@ -21,13 +21,40 @@ const Map = () =>{
     libraries: ['drawing'],
   })
 
+  
+  
+
   const [map, setMap] = React.useState(null)
+  const [polygon, setPolygon] = React.useState<google.maps.Polygon>();
 
   const onLoad = React.useCallback(function callback(map: any) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map)
+    const _polygon: google.maps.Polygon = new google.maps.Polygon({
+      paths: [
+        new google.maps.LatLng(0, 0),
+      ],
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+    });
+
+    setPolygon(_polygon);
+
+    
   }, [])
+
+  function drawLine(e: google.maps.MapMouseEvent)
+  {
+    
+    polygon!.getPaths().push(new google.maps.MVCArray([e.latLng!]))
+    //polygon.getPaths().insertAt(0, new google.maps.MVCArray([e.latLng!]));
+    polygon!.setMap(map)
+    //console.log(polygon!.getPaths().getAt(polygon!.length-1));
+  }
 
   const onUnmount = React.useCallback(function callback(map: any) {
     setMap(null)
@@ -47,6 +74,7 @@ const Map = () =>{
             position: google.maps.ControlPosition.LEFT_BOTTOM
           },
         }}
+        onClick={(e) => {drawLine(e)}}
         
       >
         { <DrawingManager/> }
